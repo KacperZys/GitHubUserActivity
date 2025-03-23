2d39c27b0ef641c77a4166a7ddfcb61a1b0ba87f
Kacper Zys <kacperzys13@gmail.com>
2025-03-23 11:29:45 +0100

    Program is working. Still need to do better exception handling.

--- GitHubUserActivity/Program.cs
+++ GitHubUserActivity/Program.cs
@@ -11,25 +11,25 @@ namespace GitHubUserActivity
             Console.WriteLine("Enter github username: ");
             string? username = Console.ReadLine();
             string url = $"https://api.github.com/users/{username}/events";
-
             try
             {
                 GetAPIResponse(url).Wait();
             }
             catch (Exception ex)
             {
-                if (ex.Message.Contains("Not Found"))
-                {
-                    Console.WriteLine("User not found");
-                    Console.WriteLine("Please enter correct username: ");
-                    username = Console.ReadLine();
-                    url = $"https://api.github.com/users/{username}/events";
-                    GetAPIResponse(url).Wait();
-                }
-                else
-                {
-                    Console.WriteLine("Error: " + ex.Message);
-                }
+                //if (ex.Message.Contains("Not Found"))
+                //{
+                //    Console.WriteLine("User not found");
+                //    Console.WriteLine("Please enter correct username: ");
+                //    username = Console.ReadLine();
+                //    url = $"https://api.github.com/users/{username}/events";
+                //    GetAPIResponse(url).Wait();
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Something went wrong. Try again.");
+                //}
+                Environment.Exit(0);
             }
         }
 
@@ -37,46 +37,74 @@ namespace GitHubUserActivity
         {
             using (HttpClient client = new HttpClient())
             {
-                client.DefaultRequestHeaders.Add("User-Agent", "MyApp/1.0");
-                HttpResponseMessage response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                List<JObject>? events = JsonConvert.DeserializeObject<List<JObject>>(jsonResponse);
-
-                if (events == null)
+                try
                 {
-                    Console.WriteLine("No data found");
-                    return;
-                }
+                    client.DefaultRequestHeaders.Add("User-Agent", "MyApp/1.0");
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
 
-                Dictionary<string, int> repoCommits = new Dictionary<string, int>();
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    List<JObject>? events = JsonConvert.DeserializeObject<List<JObject>>(jsonResponse);
 
-                foreach (var obj in events)
-                {
-                    string? type = obj["type"].ToString();
-                    var repoName = obj["repo"]["name"].ToString();
-
-                    switch (type)
+                    if (events == null)
                     {
-                        case "PushEvent":
-                            int commitsCount = Convert.ToInt32(obj["payload"]["size"]);
-                            repoCommits[repoName] = commitsCount;
-                            if (!repoCommits.ContainsKey(repoName))
-                            {
-                                repoCommits.Add(repoName, commitsCount);
-                            }
-                            else
-                            {
-                                repoCommits[repoName] += commitsCount;
-                            }
-                            break;
+                        Console.WriteLine("No data found");
+                        return;
+                    }
+
+                    Dictionary<string, int> repoCommits = new Dictionary<string, int>();
+
+                    foreach (var obj in events)
+                    {
+                        string? type = obj["type"].ToString();
+                        var repoName = obj["repo"]["name"].ToString();
+
+                        if (string.IsNullOrEmpty(type)) continue;
+
+                        switch (type)
+                        {
+                            case "PushEvent":
+                                if (!repoCommits.ContainsKey(repoName))
+                                {
+                                    repoCommits.Add(repoName, 1);
+                                }
+                                else
+                                {
+                                    repoCommits[repoName]++;
+                                }
+                                break;
+                            case "WatchEvent":
+                                Console.WriteLine($"Starred: {repoName}");
+                                Console.WriteLine();
+                                break;
+                        }
+                    }
+
+                    if (repoCommits.Count != 0)
+                    {
+                        Console.WriteLine($"Made commits in:");
+                        foreach (var repo in repoCommits)
+                        {
+                            Console.WriteLine($"Repo: {repo.Key}, Commits: {repo.Value}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("User haven't made any commits in the last 90 days.");
                     }
                 }
-
-                foreach (var repo in repoCommits)
+                catch (HttpRequestException e)
                 {
-                    Console.WriteLine($"Repo: {repo.Key}, Commits: {repo.Value}");
+                    if (e.Message.Contains("404"))
+                    {
+                        Console.WriteLine("User doesn't exist.");
+                        Console.WriteLine("Try again later.");
+                        throw new Exception("Error: " + e.Message);
+                    }
+                    else
+                    {
+                        throw new Exception("Error: " + e.Message);
+                    }
                 }
             }
         }
